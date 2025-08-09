af37b954489a170723818fca21a6408ba310b2df
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-08-09 14:56:38 +0600

    shakil

--- mall/models.py
+++ mall/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator 
+from django.core.validators import MinValueValidator, MaxValueValidator #import for ratings
 
 
 
@@ -46,7 +46,35 @@ class Rating(models.Model):
     created = models.DateTimeField(auto_now_add=True)   
         
     class Meta:
-        unique_together = ('product', 'user')
+        unique_together = ('product', 'user')  # user respect one particular product rating unique
 
     def __str__(self):
-        return f"{self.user.username} - {self.product.name} - {self.rating}"            
+        return f"{self.user.username} - {self.product.name} - {self.rating}"  
+    
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart for {self.user.username}"
+        
+    def get_total_price(self):
+        return sum(item.get_cost() for item in self.items.all())   #when a customer adds items to the cart we get price total
+
+    def get_total_item(self):
+        return sum(item.quantity for item in self.items.all())
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} X {self.product.name}"
+
+    def get_cost(self):
+        return self.product.price * self.quantity              
